903dd713fc4da4e5f77456a4f1515ed765af9c57
opinvn <opinvn@gmail.com>
2024-04-05 14:17:47 +0700

    update

--- API_Kafka_Producer/app.py
+++ API_Kafka_Producer/app.py
@@ -1,70 +1,54 @@
-# from flask import Flask, request, jsonify
-# from confluent_kafka import Producer
-# import socket
-
-# app = Flask(__name__)
-
-# # Topic Use 
-# topic_Use = 'my-topic'
-# # Connect with Kafka Producer
-# conf = {"bootstrap.servers": "kafka-service:9092", "client.id": socket.gethostname()}
-# producer = Producer(conf)
-
-# # API POST
-# @app.route('/send-message', methods=['POST'])
-# def send_message():
-#     data = request.json
-#     # Sử dụng tên chủ đề cố định
-#     key = data.get('key', 'message')  # Sử dụng giá trị mặc định 'message' nếu không được cung cấp
-#     value = data.get('value', '')     # Sử dụng chuỗi trống làm giá trị mặc định nếu không được cung cấp
-
-#     try:
-#         producer.produce(topic_Use, key=key, value=value)
-#         producer.flush()  # Đảm bảo rằng tin nhắn được gửi ngay lập tức
-#         return jsonify({'success': True, 'message': 'Message sent successfully'}), 200
-#     except Exception as e:
-#         return jsonify({'success': False, 'message': str(e)}), 500
-
-# # API GET
-# # Test: http://192.168.2.39:30002/get-message?key=test_key&value=test_value
-# @app.route('/get-message', methods=['GET'])
-# def get_message():
-
-#     # Sử dụng tên chủ đề cố định
-#     key = request.args.get('key', 'message')  # Sử dụng giá trị mặc định 'message' nếu không được cung cấp
-#     value = request.args.get('value', '')     # Sử dụng chuỗi trống làm giá trị mặc định nếu không được cung cấp
-
-#     try:
-#         producer.produce(topic_Use, key=key, value=value)
-#         producer.flush()
-#         return jsonify({'success': True, 'message': 'Message sent successfully'}), 200
-#     except Exception as e:
-#         return jsonify({'success': False, 'message': str(e)}), 500
-
-# # Run ---------------------------
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
-
+from flask import Flask, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from confluent_kafka import Producer
 import socket
 
+app = Flask(__name__)
+limiter = Limiter(app, key_func=get_remote_address)
+
+# Cấu hình Limiter
+limiter.init_app(app)
+
 # Topic Use 
 topic_Use = 'my-topic'
-
 # Connect with Kafka Producer
 conf = {"bootstrap.servers": "kafka-service:9092", "client.id": socket.gethostname()}
 producer = Producer(conf)
 
-def produce_messages():
-    while True:
-        try:
-            key = input("Enter key: ")
-            value = input("Enter value: ")
-            producer.produce(topic_Use, key=key, value=value)
-            producer.flush()  # Đảm bảo rằng tin nhắn được gửi ngay lập tức
-            print("Message sent successfully")
-        except Exception as e:
-            print(f"Failed to send message: {e}")
+# API POST
+@app.route('/send-message', methods=['POST'])
+@limiter.limit("100/second")  # Giới hạn 500 request mỗi giây
+def send_message():
+    data = request.json
+    # Sử dụng tên chủ đề cố định
+    key = data.get('key', 'message')  # Sử dụng giá trị mặc định 'message' nếu không được cung cấp
+    value = data.get('value', '')     # Sử dụng chuỗi trống làm giá trị mặc định nếu không được cung cấp
 
-if __name__ == "__main__":
-    produce_messages()
+    try:
+        producer.produce(topic_Use, key=key, value=value)
+        producer.flush()  # Đảm bảo rằng tin nhắn được gửi ngay lập tức
+        return jsonify({'success': True, 'message': 'Message sent successfully'}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)}), 500
+
+# API GET
+# Test: http://192.168.2.39:30002/get-message?key=test_key&value=test_value
+@app.route('/get-message', methods=['GET'])
+@limiter.limit("100/second")  # Giới hạn 500 request mỗi giây
+def get_message():
+
+    # Sử dụng tên chủ đề cố định
+    key = request.args.get('key', 'message')  # Sử dụng giá trị mặc định 'message' nếu không được cung cấp
+    value = request.args.get('value', '')     # Sử dụng chuỗi trống làm giá trị mặc định nếu không được cung cấp
+
+    try:
+        producer.produce(topic_Use, key=key, value=value)
+        producer.flush()
+        return jsonify({'success': True, 'message': 'Message sent successfully'}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)}), 500
+
+# Run ---------------------------
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
